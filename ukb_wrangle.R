20bc2c300180610ee41bed2ffc89181d682deda9
clries <courtknees2002@icloud.com>
2023-04-13 15:11:19 -0500

    newproj_3

--- ukb_wrangle.R
+++ ukb_wrangle.R
@@ -3,11 +3,9 @@ p = read_tsv('/wdata/cries/career_proj/biol-4386-course-project-clries_backup/da
 jobs <- p %>% 
   select(c(eid, array, job, start)) %>%
   mutate(creative = case_when(str_detect(string = job, pattern = "archit|draught|auth|reporter|music|dancer|entertain|singer|artist|arts|cartoon|painter") == TRUE ~ 1,
+                              str_detect(string = job, pattern = "farm|factory|horticult|plumb|food worker|road transport/traffic clerk|care assist") == TRUE ~ -1,
                               TRUE ~ 0)
          ) %>%
-  mutate(not_creative = case_when(str_detect(string = job, pattern = "boring") == TRUE ~ 1,
-                              TRUE ~ 0)
-  ) %>%
   rename(IID = eid)
   # select(creative) %>% table()
 
@@ -19,7 +17,7 @@ jobs <- p %>%
   file = '/wdata/lcasten/ukbb/imaging/LDPred2/LDPred2_gathered_resid_scores_w_covar.csv'
 bipolar_pgs <- read_csv(file)  %>%
   filter(pgs_name == '2021_PGC_bipolar_I_II') %>%
-  select(c(IID, adj_pgs, age_at_recruitment, sexMale))
+  select(c(IID, adj_pgs, sexMale))
 bipolar_pgs
 
 # job satisfaction
@@ -27,11 +25,20 @@ jobsat = read_csv('/wdata/cries/career_proj/biol-4386-course-project-clries/mast
 only_sat <- jobsat %>%
   select(c(eid, p4537_i0)) %>%
   rename(IID = eid)
-final <- inner_join(jobs, bipolar_pgs, by = 'IID') %>%
+all_dat <- inner_join(jobs, bipolar_pgs, by = 'IID') %>%
   inner_join(only_sat, by = 'IID') %>%
   rename(job_sat = p4537_i0)
 
+final <- all_dat %>%
+  mutate(job_sat = case_when(job_sat == 'Extremely happy' ~ 5,
+                             job_sat == 'Very happy' ~ 4,
+                             job_sat == 'Moderately happy' ~ 3,
+                             job_sat == 'Moderately unhappy' ~ 2,
+                             job_sat == 'Very unhappy' ~ 1,
+                             job_sat == 'Extremely unhappy' ~ 0)
+  )
+
 final %>%
-  select(-IID) %>%
-  head(20) %>%
-  write_csv('/wdata/cries/career_proj/tmp/dat_subset.csv')
+  select(-c(IID)) %>%
+  filter(creative != 0) %>%
+  write_csv('/wdata/cries/career_proj/tmp/biol-4386-course-project-clries/dat_subset.csv')
