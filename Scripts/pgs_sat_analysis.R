985e56dd48823ef1296212d9e9b0dfe07ea9f8f0
clries <courtney-ries@uiowa.edu>
2023-05-03 11:04:47 -0500

    more descriptive r files

--- Scripts/pgs_sat_analysis.R
+++ Scripts/pgs_sat_analysis.R
@@ -3,12 +3,16 @@
 # sex, start, array will be covariates
 
 library(tidyverse)
+
 dat = read_csv('/wdata/cries/career_proj/tmp/biol-4386-course-project-clries/Data/pgs_sat_data.csv')
+
+####### part 1: label jobs as non-creative and store in creative column as -1
+### This couldn't be done in wrangle because I chose based on rows when calling unique to the completed data
 non_creative = dat$job %>% unique() 
 nc_vec <- c(3,23,29,32,53,58,59,61,62,75,77,82,91,106,113,118,119,121,134,101,154,159,162,163,165,174,187,191,193,196,198,206,223,228,237,241,245,
             249,250,251,253,258,267,273,274,278,286,299,302,306,314,347,362,367,370,382,385,394,397,398,400,405,407,413,425,427,433,438,451,470,486,492,496,89)
 non_creative = non_creative[nc_vec]
-
+###### save noncreative jobs to a file for github
 data.frame("non_creative_jobs" = non_creative) %>% 
   write_csv('/wdata/cries/career_proj/tmp/biol-4386-course-project-clries/Data/noncreative_jobs.tsv')
 
@@ -16,9 +20,9 @@ dat2 <- dat %>%
   mutate(creative = case_when(job %in% non_creative ~ -1,
                               TRUE ~ creative)
          )
-table(dat$creative)
+# see how many individuals fall under each job category
 table(dat2$creative)
-
+####### resave data files from wrangle with noncreative data
 dat2 %>%
   select(-c(IID)) %>%
   filter(creative != 0) %>%
@@ -26,9 +30,11 @@ dat2 %>%
 dat2 %>%
   write_csv('/wdata/cries/career_proj/tmp/biol-4386-course-project-clries/Data/pgs_sat_data.csv')
 
+
+### set up descriptive label for graph
 new_label = c('-1' = "Low creativity job", '1' = "High creativity job", '0' = 'Other')
 
-
+######### Part 2: creating the graph for all 3 job categories
 dat2 %>% 
   mutate(job_sat_z = scale(job_sat)[,1]) %>%
   ggplot(aes(x = adj_pgs, y = job_sat_z)) +
@@ -43,20 +49,7 @@ dat2 %>%
         axis.title = element_text(size = 18),
         axis.text = element_text(size = 15))
 
-#using a linear model for creative jobs
-create = dat2 %>%
-  filter(creative == 1)
-lm(job_sat ~ adj_pgs + sexMale + array + start, data = create)
-#for non creative jobs
-bore = dat2 %>%
-  filter(creative == -1)
-lm(job_sat ~ adj_pgs + sexMale + array + start, data = bore)
-#for jobs that fit neither
-medium = dat2 %>%
-  filter(creative == 0)
-lm(job_sat ~ adj_pgs + sexMale + array + start, data = medium)
-
-#plot without 'other' ---- final plot
+################ Part 3: creating the graph but only for noncreative and creative
 noother <- dat2 %>% filter(creative != 0)
 
 noother %>% 
@@ -74,5 +67,20 @@ noother %>%
         axis.text = element_text(size = 15))
 
 
-# the thing for that statistic
+###################### Part 4: analysis (not included in project)
+#looking at linear model for creative jobs
+create = dat2 %>%
+  filter(creative == 1)
+lm(job_sat ~ adj_pgs + sexMale + array + start, data = create)
+#for non creative jobs
+bore = dat2 %>%
+  filter(creative == -1)
+lm(job_sat ~ adj_pgs + sexMale + array + start, data = bore)
+#for jobs that fit neither
+medium = dat2 %>%
+  filter(creative == 0)
+lm(job_sat ~ adj_pgs + sexMale + array + start, data = medium)
+
+
+# significance stats for jobsat/pgs and confounding variables
 broom::tidy(lm(job_sat ~ adj_pgs * creative + sexMale + array + start, data = dat))
--- Scripts/ukb_wrangle.R
+++ Scripts/ukb_wrangle.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+
+############### part 1: label creative jobs in a new 'creative' column as 1, and other jobs as 0
 p = read_tsv('/wdata/cries/career_proj/biol-4386-course-project-clries_backup/data/input_data/ukb_job_data.tsv')
 jobs <- p %>% 
   select(c(eid, array, job, start)) %>%
@@ -11,7 +13,7 @@ jobs <- p %>%
                           #    str_detect(string = job, pattern = ) == TRUE ~ -1,
                               TRUE ~ 0)) %>%
   rename(IID = eid)
-
+# save the list of creative jobs to a file for access on github
 p %>% 
   select(job) %>%
   filter(str_detect(string = job, pattern = "artist|music|graphic|actor,|entertain|architect|dancer|dancing|arts officer|design|
@@ -22,14 +24,15 @@ p %>%
                                          environment|bio")) %>% unique() %>%
   write_csv('/wdata/cries/career_proj/tmp/biol-4386-course-project-clries/creative_jobs.csv')
   
-  #### polygenic scores ####
+############# part 2: add BPD polygenic scores to the tibble
   file = '/wdata/lcasten/ukbb/imaging/LDPred2/LDPred2_gathered_resid_scores_w_covar.csv'
 bipolar_pgs <- read_csv(file)  %>%
   filter(pgs_name == '2021_PGC_bipolar_I_II') %>%
   select(c(IID, adj_pgs, sexMale))
 bipolar_pgs
 
-# job satisfaction
+############# part 3: add job satisfaction data, since the first file we downloaded from UKB only had job data
+####### filter to only include participants with complete job, satisfaction, and PGS data
 jobsat = read_csv('/wdata/cries/career_proj/biol-4386-course-project-clries/master_job_data_participant.csv') 
 only_sat <- jobsat %>%
   select(c(eid, p4537_i0)) %>%
@@ -37,7 +40,7 @@ only_sat <- jobsat %>%
 all_dat <- inner_join(jobs, bipolar_pgs, by = 'IID') %>%
   inner_join(only_sat, by = 'IID') %>%
   rename(job_sat = p4537_i0)
-
+## change satisfaction data from a string to a value
 final <- all_dat %>%
   mutate(job_sat = case_when(job_sat == 'Extremely happy' ~ 5,
                              job_sat == 'Very happy' ~ 4,
@@ -46,6 +49,8 @@ final <- all_dat %>%
                              job_sat == 'Very unhappy' ~ 1,
                              job_sat == 'Extremely unhappy' ~ 0)
   )
+
+################ part 5: saving tibble to a file
 ##### save a little chunk for the github
 final %>%
   select(-c(IID)) %>%
